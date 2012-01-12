4bbd1a891443dd0539bb31719284992e219f5b03
David Xu <xudavidb@gmail.com>
2012-01-12 15:13:05 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Davidxu::Application.config.secret_token = '1b2c802293c1ee336c8a75daf56b32e838a80f0202beaa4720f4a6807be120c0c76a3c7d56f9d0d8682a3bd24f7e230e53fff638b107199d49d344f3aa10b12a'
